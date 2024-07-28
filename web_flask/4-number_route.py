ee1b30941a340f97276f36edaae04c9ca8d55f3b
Praise Aribisala <aribisalapraise12@gmail.com>
2024-07-28 22:09:00 +0100

    Task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+A simple Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """
+    displays “Hello HBNB!” in the root route
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    displays "HBNB" in the root console
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """
+    display “C ”, followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
+    word = text.replace("_", " ")
+    return "C {}".format(word)
+
+
+@app.route("/python", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    """
+    display “C ”, followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
+    word = text.replace("_", " ")
+    return "Python {}".format(word)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def integer(n):
+    """
+    display “Python ”, followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
