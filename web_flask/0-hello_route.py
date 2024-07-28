0b10d0513731afa02b2cb2f30c54f055b4d360f7
Praise Aribisala <aribisalapraise12@gmail.com>
2024-07-28 16:28:14 +0100

    Task 1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,6 +4,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     """
@@ -11,8 +12,7 @@ def hello():
     """
     return "Hello HBNB!"
 
+
 if __name__ == '__main__':
     # Ensure the app listens on host '0.0.0.0' and port '5000'
     app.run(host='0.0.0.0', port=5000, debug=False)
-
-
